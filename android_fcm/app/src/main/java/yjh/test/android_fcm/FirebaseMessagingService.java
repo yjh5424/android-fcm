352226410a3e78365feec4dff43f8ec3fa7457ba
yjh5424 <yjh54240@naver.com>
2017-08-05 23:51:37 +0900

    add

--- android_fcm/app/src/main/java/yjh/test/android_fcm/FirebaseMessagingService.java
+++ android_fcm/app/src/main/java/yjh/test/android_fcm/FirebaseMessagingService.java
@@ -22,39 +22,7 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        Log.d(TAG,"From: "+remoteMessage.getFrom());
-
-        if(remoteMessage.getData().size()>0){
-            Log.d(TAG,"Message data payload "+remoteMessage.getData());
-        }
-
-        if(remoteMessage.getNotification() != null){
-            Log.d(TAG,"Message Notification Body "+remoteMessage.getNotification().getBody());
-        }
-
-        msg=remoteMessage.getNotification().getBody();
-
-        Intent intent=new Intent(this,MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-
-        PendingIntent conPendingIntent=PendingIntent.getActivity(this,0,new Intent(this,MainActivity.class),0);
-        NotificationCompat.Builder mBuilder=new NotificationCompat.Builder(this).setSmallIcon(R.mipmap.ic_launcher)
-              .setContentTitle("FCM")
-                .setAutoCancel(true)
-                .setContentText(msg)
-                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
-                .setVibrate(new long[]{1, 1000});
-
-
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        notificationManager.notify(0 /* ID of notification */, mBuilder.build());
-
-
-        mBuilder.setContentIntent(conPendingIntent);
-
-
+  
 
     }
 
